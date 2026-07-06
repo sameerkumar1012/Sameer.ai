62e0251d692a036725dfe14fd49b513d8c525f50
Sameer Kumar <sameerkumar1012@gmail.com>
2026-07-06 23:23:18 +0530

    Dockerize PortfolioGPT

--- backend/config.py
+++ backend/config.py
@@ -9,15 +9,32 @@ load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
-EMBEDDING_MODEL = "amazon.titan-embed-text-v2:0"
+EMBEDDING_MODEL = os.getenv(
+    "EMBEDDING_MODEL",
+    "amazon.titan-embed-text-v2:0"
+)
 
-LLM_MODEL = "amazon.nova-lite-v1:0"
+LLM_MODEL = os.getenv(
+    "LLM_MODEL",
+    "amazon.nova-lite-v1:0"
+)
+
+# ------------------------
+# Database
+# ------------------------
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:Sameer123@localhost:5432/portfolio_gpt"
+)
 
 # ------------------------
 # Retrieval
 # ------------------------
 
-TOP_K = 3
+TOP_K = int(os.getenv("TOP_K", 3))
+
+FAQ_THRESHOLD = float(os.getenv("FAQ_THRESHOLD", 0.65))
 
 # ------------------------
 # Vector Store
--- backend/database.py
+++ backend/database.py
@@ -5,16 +5,14 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}:"
-    f"{os.getenv('DB_PORT')}/"
-    f"{os.getenv('DB_NAME')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-Base = declarative_base()   
+Base = declarative_base()
